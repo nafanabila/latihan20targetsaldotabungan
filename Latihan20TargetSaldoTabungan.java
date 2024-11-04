56b32b214dd1e0e31a9c4c1ed8b5954f29e0d918
nafanabila <nafanabila251@gmail.com>
2024-11-04 22:35:32 +0700

    Add files via upload

---
+++ Latihan20TargetSaldoTabungan.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan20.target.saldo.tabungan;
+
+/**
+ *
+ * @author ip1pf
+ */
+public class Latihan20TargetSaldoTabungan {
+    public static void main(String[] args) {
+        // Inisialisasi variabel
+        double saldoAwal = 3500000;
+        double bungaPerBulan = 8;
+        double saldoTarget = 6000000;
+
+        // Hitung saldo tabungan setiap bulan
+        double saldoSaatIni = saldoAwal;
+        int bulan = 1;
+        while (saldoSaatIni < saldoTarget) {
+            saldoSaatIni = saldoSaatIni * (1 + bungaPerBulan / 100);
+            System.out.println("Saldo di bulan ke-" + bulan + ": Rp. " + saldoSaatIni);
+            bulan++;
+        }
+
+        // Output hasil
+        System.out.println("Untuk mencapai saldo target Rp. 6.000.000, dibutuhkan waktu " + bulan + " bulan.");
+    }
+}
